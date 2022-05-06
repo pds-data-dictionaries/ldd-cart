4c434a058da8a4678a28c01ecb5b321d6fd24820
Trent Hare <thare@usgs.gov>
2022-05-06 08:48:56 -0700

    Update conf.py
    update to home/nav page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example Namespace'
+project = 'Cartopgraphy Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
