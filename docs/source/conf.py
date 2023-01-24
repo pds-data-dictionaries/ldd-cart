e09e1428d80e0c7e7da675df5567408f973a308f
PDSEN Ops <pds-ops@jpl.nasa.gov>
2023-01-24 09:01:10 -0800

    Apply latest LDD Doc Gen Config updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Cartography Namespace'
+project = 'Example Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
@@ -88,4 +88,13 @@ html_theme_options = {
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False
+}
+
+myst_enable_extensions = [
+    "deflist",
+    "fieldlist"
+]
+
+latex_elements = {
+    'maxlistdepth': '10'
 }
