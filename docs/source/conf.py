65f69cd4a33721c07bfcae6d9f79c90e7a497bdd
Jordan Padams <33492486+jordanpadams@users.noreply.github.com>
2023-01-26 08:34:05 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example Namespace'
+project = 'Cartography Local Data Dictionary'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
